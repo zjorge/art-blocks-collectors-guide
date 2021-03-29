1226a8563cc236b887b5833710f73f78ee6051bc
Alex Supkay <asupkay1124@gmail.com>
2021-03-29 00:09:02 -0400

    Remove base account

--- src/App.js
+++ src/App.js
@@ -80,8 +80,7 @@ function App() {
 }
 
 async function requestAccount(web3, setAccount) {
-    //const accounts = await web3.eth.requestAccounts();
-    const accounts = ["0xf3860788d1597cecf938424baabe976fac87dc26"]
+    const accounts = await web3.eth.requestAccounts();
     setAccount(accounts[0]);
 }
 
