dc65bdddab69d901dfd6ca2338ff3a178a29fa1a
Alex Supkay <asupkay1124@gmail.com>
2021-04-01 17:43:22 -0400

    Add an S

--- src/components/TokenGrid.js
+++ src/components/TokenGrid.js
@@ -5,7 +5,7 @@ import './TokenGrid.css';
 function TokenGrid({tokens, setActiveToken}) {
   return (
     <div>
-      <h2 className="piece-header">Pieces Owned:</h2>
+      <h2 className="piece-header"> {tokens.length} Piece{tokens.length != 1 && 's'} Owned:</h2>
       <TokenImageGrid tokens={tokens} setActiveToken={setActiveToken}/>
     </div>
   ) 
